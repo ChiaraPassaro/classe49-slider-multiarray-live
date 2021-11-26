32b518dc139a6142cc0ef5a11d0d73236d8552de
Chiara Passaro <chiarapassaro.wd@gmail.com>
2021-11-26 08:21:11 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+/*
+ * Dati tre array contenenti 
+ * una lista ordinata di 5 immagini e 
+ * una lista ordinata dei relativi 5 luoghi,
+ * E una lista di 5 news.
+ * Creare un carosello come nell'foto allegata e
+ * al click dell'utente sulle frecce l'immagine attiva diventa 
+ * visibile in formato grande a sinistra e nel suo angolo in basso a destra 
+ * dovranno essere aggiunti il titolo e il testo.
+*/
+
+/* Hanno bisogno di:
+ */
+
+const items = [
+    'img/01.jpg',
+    'img/02.jpg',
+    'img/03.jpg',
+    'img/04.jpg',
+    'img/05.jpg'
+];
+
+const title = [
+    'Svezia',
+    'Svizzera',
+    'Gran Bretagna',
+    'Germania',
+    'Paradise'
+];
+
+const text = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+];
